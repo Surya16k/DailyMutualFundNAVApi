8d7d6f118d275c8092734b5ad404b4be00623e65
Surya16k <suryaprakash5990@gmail.com>
2022-09-14 18:25:49 +0530

    Updated

---
+++ DailyMutualFundNAV/Controllers/MutualFundNAVController.cs
@@ -0,0 +1,51 @@
+﻿using DailyMutualFundNAV.Models;
+using DailyMutualFundNAV.Repository;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DailyMutualFundNAV.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MutualFundNAVController : ControllerBase
+    {
+        private readonly IMutualFundRepository mutualFundRepository;
+       
+        public MutualFundNAVController(IMutualFundRepository _mutualFundRepository)
+        {
+            mutualFundRepository = _mutualFundRepository;
+        }
+        [HttpGet]
+        [Route("mutualfundname")]
+        public IActionResult GetMutualFundByName(string mutualfundname)
+        {
+           
+                if (mutualfundname == null)
+                {
+                    return BadRequest();
+                }
+                var mutualFundData = mutualFundRepository.GetMutualFundByNameRepository(mutualfundname);
+                if (mutualFundData == null)
+                {
+                    return BadRequest();
+                }
+                return Ok(mutualFundData);
+
+            
+        }
+        [HttpGet]
+        public ActionResult GetAllMutualFund()
+        {
+            var l = mutualFundRepository.GetAllmutualFund();
+            return Ok(l);
+
+
+        }
+
+
+    }
+}
---
+++ DailyMutualFundNAV/Repository/MutualFundRepository.cs
@@ -0,0 +1,28 @@
+﻿using DailyMutualFundNAV.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DailyMutualFundNAV.Repository
+{
+    public class MutualFundRepository:IMutualFundRepository
+    {
+        private readonly MutualFundContext db;
+        public MutualFundRepository(MutualFundContext _db)
+        {
+            db = _db;
+        }
+        public DailyMutualFundDetail GetMutualFundByNameRepository(string mutualFundName)
+        {
+            DailyMutualFundDetail mutualFundData = null;
+
+            mutualFundData = db.DailyMutualFundDetails.Where(x => x.MutualFundName == mutualFundName).FirstOrDefault();
+            return mutualFundData;
+        }
+        public List<DailyMutualFundDetail> GetAllmutualFund()
+        {
+            return db.DailyMutualFundDetails.ToList();
+        }
+    }
+}
